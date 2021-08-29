13f8d0b37206cffb6a40496bd93d764a9007def2
pewww <jung22337@naver.com>
2021-08-29 22:50:38 +0900

    :wrench: Setting of Typescript and Webpack

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.ts'
+  },
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname + '/dist')
+  },
+  resolve: {
+    extensions: [
+      '.ts',
+      '.js'
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: require.resolve('ts-loader')
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
+};
